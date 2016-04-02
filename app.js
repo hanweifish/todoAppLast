f73a483d047919f709bf1dda93f2a998a15cac4d
Victor <hanweihw@gmail.com>
2016-04-02 16:27:32 -0700

    final

--- app.js
+++ app.js
@@ -11,8 +11,8 @@ var todos = require('./routes/todos');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
